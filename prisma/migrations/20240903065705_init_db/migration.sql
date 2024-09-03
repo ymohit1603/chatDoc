21f24acd402c32fd2a76312f9be7c1b933f16c29
ymohit1603 <mohityadav0330@gmail.com>
2024-09-03 13:21:33 +0530

    add: prisma

---
+++ prisma/migrations/20240903065705_init_db/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
