c4f7f90b1b97f428e3ccfc1daa0bca9b26188a38
ymohit1603 <mohityadav0330@gmail.com>
2024-09-16 14:28:30 +0530

    add :private procedure for deleting and uploading pdf

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,8 +1,44 @@
+import prisma from '@/lib/prisma';
 import {
+  privateProcedure,
     publicProcedure,
     router,
-  } from './trpc'
+} from './trpc'
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
 
-export const appRouter = router({});
+export const appRouter = router({
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx
+    console.log('In the fucking trpc file')
+    return await prisma.pdf.findMany({
+      where: {
+        userId,
+      },
+    })
+  }),
+  deleteFile: privateProcedure
+  .input(z.object({ id: z.string() }))
+  .mutation(async ({ ctx, input }) => {
+    const { userId } = ctx
+
+    const file = await prisma.pdf.findFirst({
+      where: {
+        id: input.id,
+        userId,
+      },
+    })
+
+    if (!file) throw new TRPCError({ code: 'NOT_FOUND' })
+
+    await prisma.pdf.delete({
+      where: {
+        id: input.id,
+      },
+    })
+
+    return file
+  }),
+});
 
 export type AppRouter = typeof appRouter
