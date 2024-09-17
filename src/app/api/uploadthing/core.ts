87efb74bc3967b80069ea1fc66b84f809fffb68e
ymohit1603 <mohityadav0330@gmail.com>
2024-09-17 12:35:06 +0530

    add: pinecone,openai,langchain

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -2,6 +2,11 @@ import { getAuth } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import prisma from "@/lib/prisma";
+import { getPineconeClient } from "@/lib/pinecone";
+
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { PineconeStore } from 'langchain/vectorstores/pinecone'
 
 const f = createUploadthing();
 
@@ -52,7 +57,7 @@ export const ourFileRouter = {
         }
 
         // Create a new record in the database
-        await prisma.pdf.create({
+        const createdFile=await prisma.pdf.create({
           data: {
             key: file.key, // Correctly accessing file.key
             name: file.name, // Correctly accessing file.name
@@ -63,6 +68,53 @@ export const ourFileRouter = {
         });
 
         console.log(`File created in the database for user ${metadata.userId}`);
+
+        try {
+          const response = await fetch(
+            `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+          )
+      
+          const blob = await response.blob()
+      
+          const loader = new PDFLoader(blob)
+      
+          const pageLevelDocs = await loader.load()
+      
+          const pagesAmt = pageLevelDocs.length
+          const pinecone = await getPineconeClient()
+          const pineconeIndex = pinecone.Index('quill')
+      
+          const embeddings = new OpenAIEmbeddings({
+            openAIApiKey: process.env.OPENAI_API_KEY,
+          })
+      
+          await PineconeStore.fromDocuments(
+            pageLevelDocs,
+            embeddings,
+            {
+              pineconeIndex,
+              namespace: createdFile.id,
+            }
+          )
+      
+          await prisma.pdf.update({
+            data: {
+              uploadStatus: 'SUCCESS',
+            },
+            where: {
+              id: createdFile.id,
+            },
+          })
+        } catch (err) {
+          await prisma.pdf.update({
+            data: {
+              uploadStatus: 'FAILED',
+            },
+            where: {
+              id: createdFile.id,
+            },
+          })
+        }
       } catch (error) {
         console.error("Error during file upload handling:", error);
         throw new UploadThingError("Failed to process the uploaded file.");
